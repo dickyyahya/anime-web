e248c9d60011598e59ea367447d0e04e953ef30e
dickyyahya <dickyyahya25@gmail.com>
2026-06-22 11:11:59 +0700

    create components search

--- src/App.jsx
+++ src/App.jsx
@@ -50,17 +50,10 @@ export default function App() {
 }
 
 function NavBar() {
-  const [query, setQuery] = useState("");
-
   return (
     <nav className="nav-bar">
       <Logo />
-      <div className="search-container">
-        <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
-        <p className="search-results">
-          Found <strong>4</strong> results
-        </p>
-      </div>
+      <Search />
     </nav>
   );
 }
@@ -71,6 +64,18 @@ function Logo() {
       <span role="img">🍥</span>
       <h1>WeeBoo</h1>
       <span role="img">🍥</span>
+    </div>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <div className="search-container">
+      <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
+      <p className="search-results">
+        Found <strong>4</strong> results
+      </p>
     </div>
   );
 }
