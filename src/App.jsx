8b6f957e4259503de02d0e8c6ed208f6c5d6d4e6
dickyyahya <dickyyahya25@gmail.com>
2026-06-22 16:20:20 +0700

    add prop drilling NumResult

--- src/App.jsx
+++ src/App.jsx
@@ -44,18 +44,18 @@ export default function App() {
   const [animes, setAnimes] = useState(animesData);
 
   return (
-    <>
-      <NavBar />
+    <> 
+      <NavBar animes={animes} />
       <Main animes={animes} />
     </>
   );
 }
 
-function NavBar() {
+function NavBar({ animes }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
+      <Search animes={animes} />
     </nav>
   );
 }
@@ -70,20 +70,20 @@ function Logo() {
   );
 }
 
-function Search() {
+function Search({ animes }) {
   const [query, setQuery] = useState("");
   return (
     <div className="search-container">
       <input className="search" type="text" placeholder="Search anime..." value={query} onChange={(e) => setQuery(e.target.value)} />
-      <NumResult />
+      <NumResult animes={animes} />
     </div>
   );
 }
 
-function NumResult() {
+function NumResult({ animes }) {
   return (
     <p className="search-results">
-      Found <strong>4</strong> results
+      Found <strong>{animes.length}</strong> results
     </p>
   );
 }
