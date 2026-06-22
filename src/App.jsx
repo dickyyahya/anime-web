84b343f2a32a41e7b2296229db1c4950288ee591
dickyyahya <dickyyahya25@gmail.com>
2026-06-22 14:23:05 +0700

    create AnimeList component

--- src/App.jsx
+++ src/App.jsx
@@ -111,22 +111,26 @@ function ListBox({ animes, onSelectedAnime }) {
       <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && (
-        <ul className="list list-anime">
-          {animes?.map((anime) => (
-            <li key={anime.mal_id} onClick={() => onSelectedAnime(anime.mal_id)}>
-              <img src={anime.image} alt={`${anime.title} cover`} />
-              <h3>{anime.title}</h3>
-              <div>
-                <p>
-                  <span>{anime.year}</span>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
+      {isOpen1 && <AnimeList animes={animes} onSelectedAnime={onSelectedAnime} />}
     </div>
+  );
+}
+
+function AnimeList({ animes, onSelectedAnime }) {
+  return (
+    <ul className="list list-anime">
+      {animes?.map((anime) => (
+        <li key={anime.mal_id} onClick={() => onSelectedAnime(anime.mal_id)}>
+          <img src={anime.image} alt={`${anime.title} cover`} />
+          <h3>{anime.title}</h3>
+          <div>
+            <p>
+              <span>{anime.year}</span>
+            </p>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
 
